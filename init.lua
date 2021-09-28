0e9f5d534cfe2c52aef36a72d56981d0fd1f3a95
uavsec <uavsec@uavsec.uavsec>
2021-09-28 23:38:34 +0200

    Add initial nvim init config

---
+++ init.lua
@@ -0,0 +1,166 @@
+-------------------- HELPERS -------------------------------
+local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
+local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
+local g = vim.g      -- a table to access global variables
+local opt = vim.opt  -- to set options
+
+local function map(mode, lhs, rhs, opts)
+  local options = {noremap = true}
+  if opts then options = vim.tbl_extend('force', options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+-------------------- PLUGINS -------------------------------
+cmd 'packadd paq-nvim'               -- load the package manager
+local paq = require('paq-nvim').paq  -- a convenient alias
+paq {'savq/paq-nvim', opt = true}    -- paq-nvim manages itself
+
+-- Coding related
+paq {'neovim/nvim-lspconfig'}
+paq {'hrsh7th/nvim-compe'}
+paq {'nvim-treesitter/nvim-treesitter'}
+
+--- Syntax
+paq {'windwp/nvim-autopairs'}
+
+--- Visual aid
+paq {'lewis6991/gitsigns.nvim'}
+paq {'lukas-reineke/indent-blankline.nvim'}
+
+-- Search related
+paq {'nvim-telescope/telescope.nvim'}
+paq {'nvim-lua/plenary.nvim'}
+
+-- UI visuals
+paq {'marko-cerovac/material.nvim'}
+paq {'hoob3rt/lualine.nvim'}
+
+-------------------- TREE-SITTER ---------------------------
+local ts = require 'nvim-treesitter.configs'
+ts.setup {ensure_installed = 'maintained', highlight = {enable = true}}
+
+-------------------- LSP -----------------------------------
+local lsp = require 'lspconfig'
+
+-- We use the default settings for pylsp: the option table can stay empty
+lsp.pylsp.setup {}
+
+map('n', '<space>,', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
+map('n', '<space>;', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
+map('n', '<space>a', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+map('n', '<space>d', '<cmd>lua vim.lsp.buf.definition()<CR>')
+map('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+map('n', '<space>h', '<cmd>lua vim.lsp.buf.hover()<CR>')
+map('n', '<space>m', '<cmd>lua vim.lsp.buf.rename()<CR>')
+map('n', '<space>r', '<cmd>lua vim.lsp.buf.references()<CR>')
+map('n', '<space>s', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+
+-------------------- LSP COMPE -----------------------------
+local cmp = require 'compe'
+
+cmp.setup{
+  enabled = true,
+  autocomplete = true;
+  debug = false;
+  min_length = 1;
+  preselect = "always",
+  throttle_time = 80,
+  source_timeout = 200,
+  resolve_timeout = 800,
+  incomplete_delay = 400,
+  max_abbr_width = 100,
+  max_kind_width = 100,
+  max_menu_width = 100,
+
+  source = {
+    path = true;
+    buffer = true;
+    calc = true;
+    nvim_lsp = true;
+    nvim_lua = true;
+    vsnip = true;
+    ultisnips = true;
+    luasnip = true;
+    tresitter = true;
+  },
+}
+
+-------------------- GITSIGNS ------------------------------
+local git = require 'gitsigns'
+
+git.setup{}
+
+-------------------- PAIRS ---------------------------------
+local pairs = require 'nvim-autopairs'
+
+pairs.setup{}
+
+-------------------- INDENT-BLANKLINE ----------------------
+local indent = require 'indent_blankline'
+
+indent.setup{
+  space_char_blankline = " ",
+  show_current_context = true,
+}
+
+-------------------- LUALINE -------------------------------
+local line = require 'lualine'
+
+line.setup {
+  options = {
+    icons_enabled = true,
+    theme = 'material-nvim',
+    --component_separators = {'', ''},
+    --section_separators = {'', ''},
+    disabled_filetypes = {}
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {}
+}
+
+-------------------- COMMANDS ------------------------------
+cmd 'au TextYankPost * lua vim.highlight.on_yank {on_visual = false}'  -- disabled in visual mode
+
+-------------------- MATERIAL THEME -------------------------
+local mat = require 'material'
+
+g.material_style = 'darker'
+cmd[[colorscheme material]]
+mat.setup{}
+
+-------------------- OPTIONS -------------------------------
+opt.expandtab = true                -- Use spaces instead of tabs
+opt.hidden = true                   -- Enable background buffers
+opt.ignorecase = true               -- Ignore case
+opt.joinspaces = false              -- No double spaces with join
+opt.list = true                     -- Show some invisible characters
+opt.number = true                   -- Show line numbers
+opt.relativenumber = true           -- Relative line numbers
+opt.scrolloff = 4                   -- Lines of context
+opt.shiftround = true               -- Round indent
+opt.shiftwidth = 2                  -- Size of an indent
+opt.sidescrolloff = 8               -- Columns of context
+opt.smartcase = true                -- Do not ignore case with capitals
+opt.smartindent = true              -- Insert indents automatically
+opt.splitbelow = true               -- Put new windows below current
+opt.splitright = true               -- Put new windows right of current
+opt.tabstop = 2                     -- Number of spaces tabs count for
+opt.termguicolors = true            -- True color support
+opt.wildmode = {'list', 'longest'}  -- Command-line completion mode
+opt.wrap = false                    -- Disable line wrap
