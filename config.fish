8f20d0a148e800698618377d01707575907e40db
uavsec <36814813+uavsec@users.noreply.github.com>
2021-11-08 11:58:32 +0300

    Delete config.fish

--- config.fish
+++
@@ -1,6 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-# Start docker in background and disowned (WSL lack of systemctl hack)
-alias kit="sudo dockerd &"
